fffe648a1f56f545cc169e021b05c926b8b4dbd5
pyoung <pmmh9395@gmail.com>
2024-02-27 12:28:42 +0900

    JavaPractice.java 생성

--- JavaPractice.java
+++ JavaPractice.java
@@ -1,5 +1,16 @@
 public class JavaPractice {
     public static void main(String[] args) {
         System.out.println(" Hello Java  ");
+
+        //피자 나눠먹기(1)
+        class Solution{
+            public int solution(int n){
+                int result;
+                return result = (n%7==0) ? n/7 : n/7+1;
+            }
+        }
+
+
+
     }
 }
