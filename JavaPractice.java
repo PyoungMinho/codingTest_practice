0b1105861c6fb93d3f840bb8ee6599e9c7d05afd
pyoung <pmmh9395@gmail.com>
2024-02-27 12:47:50 +0900

    코딩테스트 연습중

--- JavaPractice.java
+++ JavaPractice.java
@@ -14,7 +14,7 @@ public class JavaPractice {
         return result = (n%7==0) ? n/7 : n/7+1;
     }
 
-    //머쓱이네 피자가게는 피자를 여섯 조각으로 잘라 줍니다.
+    // 머쓱이네 피자가게는 피자를 여섯 조각으로 잘라 줍니다.
     // 피자를 나눠먹을 사람의 수 n이 매개변수로 주어질 때, n명이 주문한 피자를 남기지 않고
     // 모두 같은 수의 피자 조각을 먹어야 한다면 최소 몇 판을 시켜야 하는지를 return 하도록 solution 함수를 완성해보세요.
     public static int solution2(int n){
@@ -26,5 +26,12 @@ public class JavaPractice {
         }
 
         return pizza;
+    }
+
+    // 머쓱이네 피자가게는 피자를 두 조각에서 열 조각까지 원하는 조각 수로 잘라줍니다.
+    // 피자 조각 수 slice와 피자를 먹는 사람의 수 n이 매개변수로 주어질 때, n명의 사람이 최소 한 조각 이상 피자를 먹으려면
+    // 최소 몇 판의 피자를 시켜야 하는지를 return 하도록 solution 함수를 완성해보세요.
+    public static int solution3(int  slice, int n){
+     return n%slice>0 ? n/slice+1 : n/slice;
     }
 }
