12726082bb78b17e5ad9bc9f3100ddb35850cbb8
Pyoung MinHo <149047097+PyoungMinho@users.noreply.github.com>
2024-03-14 16:00:26 +0900

    [level 0] Title: 접미사인지 확인하기, Time: 0.03 ms, Memory: 75.5 MB -BaekjoonHub

--- 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.java
+++ 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.java
@@ -1,14 +1,5 @@
-import java.util.Arrays;
-
 class Solution {
     public int solution(String my_string, String is_suffix) {
-        String[] strArr = new String[my_string.length()]; 
-        
-        for(int i =0; i< my_string.length(); i++){ 
-             strArr[i] = my_string.substring(i,my_string.length());
-        }
-        
-        return Arrays.asList(strArr).contains(is_suffix) ? 1 :0 ;
+        return my_string.endsWith(is_suffix) ? 1 : 0;
     }
 }
-
