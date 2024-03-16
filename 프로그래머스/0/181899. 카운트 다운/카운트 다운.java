e17105ce0f18e07e1d5750c1216e1860af1a10e9
Pyoung MinHo <149047097+PyoungMinho@users.noreply.github.com>
2024-03-16 15:17:30 +0900

    [level 0] Title: 카운트 다운, Time: 0.01 ms, Memory: 81.1 MB -BaekjoonHub

--- 프로그래머스/0/181899. 카운트 다운/카운트 다운.java
+++ 프로그래머스/0/181899. 카운트 다운/카운트 다운.java
@@ -1,16 +1,8 @@
 class Solution {
     public int[] solution(int start, int end) {
-        int[] answer;
-        if (start >= end) {
-            answer = new int[start - end + 1];
-            for (int i = 0; i < answer.length; i++) {
-                answer[i] = start - i;
-            }
-        } else {
-            answer = new int[end - start + 1];
-            for (int i = 0; i < answer.length; i++) {
-                answer[i] = start + i;
-            }
+        int[] answer = new int[start -end +1];
+        for(int i=0; i<answer.length; i++){
+            answer[i] = start -i;
         }
         return answer;
     }
