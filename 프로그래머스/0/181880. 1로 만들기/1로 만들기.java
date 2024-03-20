703c7f996ecf49b005522ad292b35a8e6631bf79
Pyoung MinHo <149047097+PyoungMinho@users.noreply.github.com>
2024-03-20 09:37:31 +0900

    [level 0] Title: 1로 만들기, Time: 2.02 ms, Memory: 82.9 MB -BaekjoonHub

--- 프로그래머스/0/181880. 1로 만들기/1로 만들기.java
+++ 프로그래머스/0/181880. 1로 만들기/1로 만들기.java
@@ -1,19 +1,11 @@
-public class Solution {
-    public int solution(int[] num_list) {
-        int answer = 0;
+import java.util.Arrays;
 
-        for(int i = 0; i < num_list.length; i++) {
-            int num = num_list[i];
-            while (num != 1) {
-                if (num % 2 == 0) {
-                    num /= 2;
-                } else {
-                    num -= 1;
-                    num /= 2;
-                }
-                answer++;
-            }
-        }
-        return answer;
+class Solution {
+    public int solution(int[] num_list) {
+        // 정수 배열을 스트림으로 변환하고, 각 숫자를 이진수로 변환한 후 가장 왼쪽에 위치한 1의 위치를 계산하고,
+        // 이를 모두 더하여 반환
+        return Arrays.stream(num_list)
+                     .map(i -> Integer.toBinaryString(i).length() - 1) // 이진수의 길이에서 1을 뺀 값을 반환
+                     .sum(); // 모든 연산 결과를 합산하여 반환
     }
 }
