34b277d0f4af6b213977884128946f649a2330f0
Pyoung MinHo <149047097+PyoungMinho@users.noreply.github.com>
2024-03-26 16:31:31 +0900

    [level 0] Title: 정수 부분, Time: 0.01 ms, Memory: 85.2 MB -BaekjoonHub

--- 프로그래머스/0/181850. 정수 부분/정수 부분.java
+++ 프로그래머스/0/181850. 정수 부분/정수 부분.java
@@ -1,8 +1,5 @@
-import java.lang.Math;
-
 class Solution {
     public int solution(double flo) {
-        int answer = (int)Math.floor(flo);
-        return answer;
+        return (int)flo;
     }
 }
