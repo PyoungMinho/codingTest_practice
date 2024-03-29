e98649fd2de5d4dcb560d0fdad48c7dac9ff523d
Pyoung MinHo <149047097+PyoungMinho@users.noreply.github.com>
2024-03-29 12:56:59 +0900

    [level 0] Title: 커피 심부름, Time: 0.03 ms, Memory: 73.2 MB -BaekjoonHub

--- 프로그래머스/0/181837. 커피 심부름/커피 심부름.java
+++ 프로그래머스/0/181837. 커피 심부름/커피 심부름.java
@@ -1,15 +1,11 @@
 class Solution {
     public int solution(String[] order) {
-        int americanoCount = 0;
-        int cafeLatteCount = 0;
-        
-       for(int i = 0; i < order.length; i++) {
-            if(order[i].contains("americano") || order[i].equals("anything")) {
-                americanoCount++;
-            } else if(order[i].contains("cafelatte")) {
-                cafeLatteCount++;
-            }
+        int answer = 0;
+        for(String item : order){
+            if(item.contains("cafelatte")){
+                answer += 5000;
+            }else answer += 4500;
         }
-        return (americanoCount *4500)+(cafeLatteCount *5000);
+        return answer;
     }
 }
