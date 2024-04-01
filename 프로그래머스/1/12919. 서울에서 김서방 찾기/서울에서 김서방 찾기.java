0a1a5e2a8f05789ccb931301f5b3ac4ce5d28dfe
Pyoung MinHo <149047097+PyoungMinho@users.noreply.github.com>
2024-04-01 13:57:03 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 1.35 ms, Memory: 77.5 MB -BaekjoonHub

--- 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
@@ -1,14 +1,13 @@
+import java.lang.*;
 class Solution {
     public String solution(String[] seoul) {
         int x = 0;
-        for(int i=0; i<seoul.length; i++){
-            if(seoul[i].equals("Kim")){
+        for (int i = 0; i < seoul.length; i++) {
+            if (seoul[i].equals("Kim")) {
                 x = i;
-                break;
+                break; 
             }
         }
-        System.out.printf("김서방은 %d에 있다",x);
-        
-        return "김서방은 " + x + "에 있다";
+         return String.format("김서방은 %d에 있다", x);
     }
 }
