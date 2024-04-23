9d64050c116cd05f0920732090dc2b6323c063ce
Pyoung MinHo <149047097+PyoungMinho@users.noreply.github.com>
2024-04-23 18:08:39 +0900

    [level 0] Title: 짝수는 싫어요, Time: 1.95 ms, Memory: 80.7 MB -BaekjoonHub

--- 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.java
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -1,11 +1,14 @@
+import java.util.List;
+import java.util.ArrayList;
+
 class Solution {
     public int[] solution(int n) {
-        int[] answer = new int[(n+1) /2];
-        
-        for(int i=0; i< answer.length; i++){
-            answer[i] = 2*i+1;
+        List<Integer> answer = new ArrayList<>();
+        for(int i=1; i<=n; i++){
+            if(i % 2 == 1){
+                answer.add(i);
+            }
         }
-        
-        return answer;
+        return answer.stream().mapToInt(x -> x).toArray();
     }
 }
