568106244489fc79d2676dd16e0dbc1f5571714e
Pyoung MinHo <149047097+PyoungMinho@users.noreply.github.com>
2024-04-25 09:35:57 +0900

    [level 0] Title: k의 개수, Time: 415.69 ms, Memory: 409 MB -BaekjoonHub

--- 프로그래머스/0/120887. k의 개수/k의 개수.java
+++ 프로그래머스/0/120887. k의 개수/k의 개수.java
@@ -1,24 +1,10 @@
-import java.util.*;
-
 class Solution {
     public int solution(int i, int j, int k) {
-        int answer = 0;
-        char target = (char) (k + '0');
-
-        
         String str = "";
-        for(int o=i; o<=j; o++){
-            str += o;
+        for(int m = i; m <= j; m++) {
+            str += m+"";
         }
-        
-        char[] chArr =str.toCharArray();
-        for(char ch : chArr){
-            if(target == ch){
-                answer ++;
-            }
-        }
-    
-        
-        return answer;
+
+        return str.length() - str.replace(k+"", "").length();
     }
 }
