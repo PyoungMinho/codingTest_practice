0633efdce20fa761bed0bfe799184a8290ff51ef
Pyoung MinHo <149047097+PyoungMinho@users.noreply.github.com>
2024-04-26 14:39:02 +0900

    [level 0] Title: 피자 나눠 먹기 (1), Time: 0.02 ms, Memory: 76.1 MB -BaekjoonHub

--- 프로그래머스/0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.java
+++ 프로그래머스/0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.java
@@ -1,10 +1,5 @@
 class Solution {
     public int solution(int n) {
-        if(n<=7){
-            return 1;
-        }else if (n%7==0){
-            return n/7;
-        }else   
-            return n/7+1;
+        return (n + 6) / 7;
     }
 }
