b885ae988e0016b5317abd3a7a999705e0ed1213
Pyoung MinHo <149047097+PyoungMinho@users.noreply.github.com>
2024-05-02 13:26:13 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.02 ms, Memory: 76.3 MB -BaekjoonHub

--- 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.java
+++ 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.java
@@ -1,15 +1,12 @@
 class Solution {
     public int solution(String my_string) {
         int answer = 0;
-        
-        char[] str = my_string.toCharArray();
-        
-        for(char ch : str){
-            if(48<=ch && ch <=57){
-                answer+=ch-'0';
+
+        for(char c : my_string.toCharArray()){
+            if(c >= '0' && c <= '9'){
+                answer += c - '0';
             }
         }
-        
         return answer;
     }
 }
