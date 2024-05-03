04bb28d1a385a897fe9bc57c7113608eca98df3a
Pyoung MinHo <149047097+PyoungMinho@users.noreply.github.com>
2024-05-03 13:43:46 +0900

    [level 0] Title: 암호 해독, Time: 1.34 ms, Memory: 78.5 MB -BaekjoonHub

--- 프로그래머스/0/120892. 암호 해독/암호 해독.java
+++ 프로그래머스/0/120892. 암호 해독/암호 해독.java
@@ -1,14 +1,9 @@
 class Solution {
     public String solution(String cipher, int code) {
         String answer = "";
-        int idx = cipher.length() / code;
-        
-        for(int i=1; i<=idx; i++){
-            
-                answer += cipher.charAt(i*code-1);
-                        
+        for(int i=code-1; i<cipher.length(); i+=code){
+            answer += cipher.substring(i, i+1);
         }
-        
         return answer;
     }
 }
