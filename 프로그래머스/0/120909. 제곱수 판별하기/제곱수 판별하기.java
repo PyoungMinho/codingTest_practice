2d07c47c2fd0d76fcd81f39286ad00ff373fe7ef
Pyoung MinHo <149047097+PyoungMinho@users.noreply.github.com>
2024-05-05 15:19:52 +0900

    [level 0] Title: 제곱수 판별하기, Time: 0.02 ms, Memory: 68.4 MB -BaekjoonHub

--- 프로그래머스/0/120909. 제곱수 판별하기/제곱수 판별하기.java
+++ 프로그래머스/0/120909. 제곱수 판별하기/제곱수 판별하기.java
@@ -1,9 +1,6 @@
 class Solution {
     public int solution(int n) {
-        if (n % Math.sqrt(n) == 0) {
-            return 1;
-        } else {
-            return 2;
-        }
+        int answer = 0;
+        return Math.sqrt(n) % 1 == 0 ? 1 : 2;
     }
 }
