c7e2d29851ff1891e45c85e9d2c00707b3c32d6b
Pyoung MinHo <149047097+PyoungMinho@users.noreply.github.com>
2024-05-08 10:45:32 +0900

    [level 0] Title: 최댓값 만들기 (1), Time: 0.52 ms, Memory: 75.1 MB -BaekjoonHub

--- 프로그래머스/0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.java
+++ 프로그래머스/0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.java
@@ -1,15 +1,11 @@
+import java.util.*;
+
 class Solution {
     public int solution(int[] numbers) {
-        int answer = numbers[0] * numbers[1];
-        
-        for (int i = 0; i < numbers.length; i++) {
-            for (int k = 0; k < numbers.length; k++) {
-                if (i != k && numbers[i] * numbers[k] > answer) {
-                    answer = numbers[i] * numbers[k];
-                }
-            }
-        }
-        
-        return answer;
+        int answer = 0;
+
+        Arrays.sort(numbers);
+
+        return numbers[numbers.length-1]*numbers[numbers.length-2];
     }
 }
