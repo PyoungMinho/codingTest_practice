1c78dbf8fab48f8e795a43d4d8a22a6e042441b1
Pyoung MinHo <149047097+PyoungMinho@users.noreply.github.com>
2024-05-13 11:21:50 +0900

    [level 0] Title: OX퀴즈, Time: 0.11 ms, Memory: 76.8 MB -BaekjoonHub

--- 프로그래머스/0/120907. OX퀴즈/OX퀴즈.java
+++ 프로그래머스/0/120907. OX퀴즈/OX퀴즈.java
@@ -1,18 +1,14 @@
 class Solution {
     public String[] solution(String[] quiz) {
-		String[] answer = new String[quiz.length];
-        
-		for (int i = 0; i < quiz.length; i++) {
-
-			String arr[] = quiz[i].split(" ");
-
-			int left = arr[1].equals("+") ? Integer.parseInt(arr[0]) + Integer.parseInt(arr[2])
-					: Integer.parseInt(arr[0]) - Integer.parseInt(arr[2]);
+        for(int i=0; i<quiz.length; i++){
             
-			int right = Integer.parseInt(arr[4]);
-
-			answer[i] = left == right ? "O" : "X";
-		}
-		return answer;
-	}
+            String[] text = quiz[i].split(" ");
+            int result = Integer.parseInt(text[0]) + 
+                        ( Integer.parseInt(text[2]) * (text[1].equals("+") ? 1:-1) );
+            quiz[i] = result == Integer.parseInt(text[4])? "O": "X";
+            
+        }
+        
+        return quiz;
+    }
 }
