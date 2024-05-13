3d6e6249e31bc35522a12d454aec47454b35a1d0
Pyoung MinHo <149047097+PyoungMinho@users.noreply.github.com>
2024-05-13 11:15:25 +0900

    [level 0] Title: 이진수 더하기, Time: 0.04 ms, Memory: 77 MB -BaekjoonHub

--- 프로그래머스/0/120885. 이진수 더하기/이진수 더하기.java
+++ 프로그래머스/0/120885. 이진수 더하기/이진수 더하기.java
@@ -1,9 +1,5 @@
 class Solution {
     public String solution(String bin1, String bin2) {
-        int sum = Integer.parseInt(bin1, 2)+Integer.parseInt(bin2,2);
-       
-        String answer = Integer.toBinaryString(sum);
-        
-        return answer;
+        return Integer.toString(Integer.parseInt(bin1, 2) + Integer.parseInt(bin2, 2),2);
     }
 }
