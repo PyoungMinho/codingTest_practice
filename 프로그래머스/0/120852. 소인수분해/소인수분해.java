8fc2d92fab4b49dae45134d9893333142007b512
Pyoung MinHo <149047097+PyoungMinho@users.noreply.github.com>
2024-05-14 09:27:30 +0900

    [level 0] Title: 소인수분해, Time: 2.89 ms, Memory: 79.9 MB -BaekjoonHub

--- 프로그래머스/0/120852. 소인수분해/소인수분해.java
+++ 프로그래머스/0/120852. 소인수분해/소인수분해.java
@@ -1,17 +1,18 @@
 import java.util.*;
-
 class Solution {
-    public int[] solution(int n) {
-        List<Integer> factorList = new ArrayList<>();
-        for (int i = 2; i <= n; i++) {
-            if (n % i == 0) {
-                factorList.add(i);
-                while (n % i == 0) {
-                    n /= i;
-                }
+    public int[] solution(int n) {    
+        Set<Integer> set = new TreeSet<>();
+
+        int temp = n;
+        for(int i=2; i<=n/2; i++) {
+            while (temp%i == 0){    
+                temp /= i;   
+                set.add(i);
             }
+            if (n == 1) break;
         }
-        int[] answer = factorList.stream().mapToInt(i -> i).toArray();
-        return answer;
+
+        if (set.size() == 0) return new int[] {n};
+        else return set.stream().mapToInt(Integer::intValue).toArray();
     }
 }
