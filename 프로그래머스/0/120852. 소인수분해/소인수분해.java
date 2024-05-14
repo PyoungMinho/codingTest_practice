50a6752033b0c5138ab2c5d329c3aa118061bb26
Pyoung MinHo <149047097+PyoungMinho@users.noreply.github.com>
2024-05-14 09:25:22 +0900

    [level 0] Title: 소인수분해, Time: 2.02 ms, Memory: 75.2 MB -BaekjoonHub

--- 프로그래머스/0/120852. 소인수분해/소인수분해.java
+++ 프로그래머스/0/120852. 소인수분해/소인수분해.java
@@ -2,22 +2,16 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int n) {
-        ArrayList<Integer> arrays = new ArrayList<>();
-        
-        for(int i =2; i<=n; i++) {
-           if(n % i == 0) {
-                while(n%i==0) {
-                    n /= i ;
+        List<Integer> factorList = new ArrayList<>();
+        for (int i = 2; i <= n; i++) {
+            if (n % i == 0) {
+                factorList.add(i);
+                while (n % i == 0) {
+                    n /= i;
                 }
-               arrays.add(i);
-            }            
+            }
         }
-        
-        // ArrayList를 배열로 변환
-        int[] answer = new int[arrays.size()];
-        for(int i = 0; i < arrays.size(); i++) {
-            answer[i] = arrays.get(i);
-        }
+        int[] answer = factorList.stream().mapToInt(i -> i).toArray();
         return answer;
     }
 }
