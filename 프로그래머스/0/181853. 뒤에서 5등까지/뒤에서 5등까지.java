3081d055efcc07f1bd53a4f843ca1d8a1d550eb6
Pyoung MinHo <149047097+PyoungMinho@users.noreply.github.com>
2024-05-19 13:54:36 +0900

    [level 0] Title: 뒤에서 5등까지, Time: 0.45 ms, Memory: 75.2 MB -BaekjoonHub

--- 프로그래머스/0/181853. 뒤에서 5등까지/뒤에서 5등까지.java
+++ 프로그래머스/0/181853. 뒤에서 5등까지/뒤에서 5등까지.java
@@ -2,14 +2,8 @@ import java.util.Arrays;
 
 class Solution {
     public int[] solution(int[] num_list) {
-        int[] answer = new int[5];
-        
         Arrays.sort(num_list);
-        
-        for(int i=0; i<answer.length; i++){
-            answer[i] = num_list[i];
-        }
-        
-        return answer;
+
+        return Arrays.copyOfRange(num_list, 0, 5);
     }
 }
