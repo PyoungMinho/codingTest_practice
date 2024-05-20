89480b2471fdf905336b7d62bc0c93bd0b751ed7
Pyoung MinHo <149047097+PyoungMinho@users.noreply.github.com>
2024-05-20 12:57:32 +0900

    [level 0] Title: 숫자 찾기, Time: 0.03 ms, Memory: 73.3 MB -BaekjoonHub

--- 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.java
+++ 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.java
@@ -1,19 +1,9 @@
 class Solution {
     public int solution(int num, int k) {
-        int answer = 0;
-        
-        String str = num+"";
-        String[] strArr = str.split("");
-        
-        for(int i=0; i<strArr.length; i++){
-            if(strArr[i].equals(k+"")){
-                answer = i+1;
-                    break;
-            }
-            else answer = -1;
-        }
-        
-        
-        return answer;
+        String numStr = String.valueOf(num);
+        String kStr = String.valueOf(k);
+
+        int answer = numStr.indexOf(kStr);
+        return answer < 0 ? -1 : answer + 1 ;
     }
 }
