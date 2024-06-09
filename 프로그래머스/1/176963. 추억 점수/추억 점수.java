03929cc617a979d42f99496e4bb6be6057acca2c
Pyoung MinHo <149047097+PyoungMinho@users.noreply.github.com>
2024-06-09 09:23:59 +0900

    [level 1] Title: 추억 점수, Time: 0.02 ms, Memory: 73.1 MB -BaekjoonHub

--- 프로그래머스/1/176963. 추억 점수/추억 점수.java
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.java
@@ -1,27 +1,14 @@
-
-import java.util.HashMap;
-import java.util.Map;
-
 class Solution {
     public int[] solution(String[] name, int[] yearning, String[][] photo) {
         int[] answer = new int[photo.length];
-        Map<String, Integer> map = new HashMap<>();
         
-       for (int i = 0; i < name.length; i++) {
-            map.put(name[i], yearning[i]); // 이름과 나이를 맵에 추가
-        }
-            
-        
-        for(int i=0; i<photo.length; i++){
-            int sum = 0;
-            
-            for(int j=0; j<photo[i].length; j++){
-                Integer friend = map.get(photo[i][j]); //map에 해당 키값이 있는 경우 
+        for(int i = 0; i < photo.length; i++){
+            for(int j = 0; j < photo[i].length; j++){
+                for(int k = 0; k < name.length; k++){
                     
-                if (friend != null) { sum += friend; } // 존재하는 경우 나이 합산 
+                    if(photo[i][j].equals(name[k])) answer[i] += yearning[k];
+                }
             }
-            
-            answer[i] = sum; // 사진마다의 나이 합계를 answer에 배열 저장
         }
         return answer;
     }
