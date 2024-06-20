ff9f40aa13396461c0011d33d39fdf712718dde2
Pyoung MinHo <149047097+PyoungMinho@users.noreply.github.com>
2024-06-20 13:05:56 +0900

    [level 0] Title: 캐릭터의 좌표, Time: 0.02 ms, Memory: 81.7 MB -BaekjoonHub

--- 프로그래머스/0/120861. 캐릭터의 좌표/캐릭터의 좌표.java
+++ 프로그래머스/0/120861. 캐릭터의 좌표/캐릭터의 좌표.java
@@ -1,22 +1,12 @@
 class Solution {
     public int[] solution(String[] keyinput, int[] board) {
-        int[] answer = {0, 0};
-        
-        for (String s : keyinput) {
-            if (s.equals("left") && answer[0] > -(board[0] / 2)) {
-                answer[0] -= 1;
+        int[] now = {0, 0};
+        for (int i = 0; i < keyinput.length; i++){
+            if(keyinput[i].equals("left")) now[0] -= now[0]>-(board[0]/2)?1:0;
+            else if(keyinput[i].equals("right")) now[0] += now[0]<(board[0]/2)?1:0;
+            else if(keyinput[i].equals("down")) now[1] -= now[1]>-(board[1]/2)?1:0;
+            else if(keyinput[i].equals("up")) now[1] += now[1]<(board[1]/2)?1:0;
             }
-            if (s.equals("right") && answer[0] < board[0] / 2) {
-                answer[0] += 1;
-            }
-            if (s.equals("up") && answer[1] < board[1] / 2) {
-                answer[1] += 1;
-            }
-            if (s.equals("down") && answer[1] > -(board[1] / 2)) {
-                answer[1] -= 1;
-            }
-        }
-        
-        return answer;
+        return now;
     }
 }
