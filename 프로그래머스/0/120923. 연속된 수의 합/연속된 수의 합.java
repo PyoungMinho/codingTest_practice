548ccc92178cbaec8c3faea09db334d8ed71125c
Pyoung MinHo <149047097+PyoungMinho@users.noreply.github.com>
2024-07-03 14:37:47 +0900

    [level 0] Title: 연속된 수의 합, Time: 0.02 ms, Memory: 83.6 MB -BaekjoonHub

--- 프로그래머스/0/120923. 연속된 수의 합/연속된 수의 합.java
+++ 프로그래머스/0/120923. 연속된 수의 합/연속된 수의 합.java
@@ -1,11 +1,15 @@
+
 class Solution {
-        public int[] solution(int num, int total) {
-                int[] answer = new int[num];
-                int check = num*(num+1) / 2;
-                int start = (total - check) / num + 1;
-                for (int i = 0; i < answer.length; i++) {
-                        answer[i] = start + i ;
-                }
-                return answer;
+    public int[] solution(int num, int total) {
+        int[] answer = new int[num];
+        int mid = (num - 1) / 2;
+
+        answer[mid] = total / num;
+
+        for (int i = 0; i < num; i++) {
+            answer[i] = answer[mid] + (i - mid);
         }
+
+        return answer;
+    }
 }
