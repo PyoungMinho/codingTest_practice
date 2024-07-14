d6babdfb1143161a3a0df133ff3f2d8241b6174e
Pyoung MinHo <149047097+PyoungMinho@users.noreply.github.com>
2024-07-14 14:01:11 +0900

    [level 0] Title: 카운트 업, Time: 0.04 ms, Memory: 81.4 MB -BaekjoonHub

--- 프로그래머스/0/181920. 카운트 업/카운트 업.java
+++ 프로그래머스/0/181920. 카운트 업/카운트 업.java
@@ -1,10 +1,9 @@
 class Solution {
-    public int[] solution(int start_num, int end_num) {
-        int[] answer = new int[end_num-start_num +1];
+    public int[] solution(int start, int end) {
+        int[] answer = new int[end - start+1];
         
-        for(int i=0; i<answer.length; i++){
-            answer[i]= start_num;
-            start_num++;
+        for(int i =0; i<= end - start; i++) {
+            answer[i] = start + i;
         }
         return answer;
     }
