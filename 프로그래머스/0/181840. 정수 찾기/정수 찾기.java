006afb300877714c359a311c754f3478ed911385
Pyoung MinHo <149047097+PyoungMinho@users.noreply.github.com>
2024-07-30 13:08:50 +0900

    [level 0] Title: 정수 찾기, Time: 2.63 ms, Memory: 82.9 MB -BaekjoonHub

--- 프로그래머스/0/181840. 정수 찾기/정수 찾기.java
+++ 프로그래머스/0/181840. 정수 찾기/정수 찾기.java
@@ -1,10 +1,7 @@
+import java.util.stream.IntStream;
+
 class Solution {
-    public int solution(int[] num_list, int n) {
-        int answer = 0;
-        for(int i : num_list ){
-            if(i == n){answer = 1;}
-        }
-        
-        return answer;
+    public int solution(int[] numList, int n) {
+        return IntStream.of(numList).anyMatch(num -> num == n) ? 1 : 0;
     }
 }
