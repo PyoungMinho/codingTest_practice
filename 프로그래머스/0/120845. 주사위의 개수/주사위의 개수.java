77a386523459390985227900cd1781e5f877e338
Pyoung MinHo <149047097+PyoungMinho@users.noreply.github.com>
2024-07-30 13:09:10 +0900

    [level 0] Title: 주사위의 개수, Time: 0.04 ms, Memory: 77.6 MB -BaekjoonHub

--- 프로그래머스/0/120845. 주사위의 개수/주사위의 개수.java
+++ 프로그래머스/0/120845. 주사위의 개수/주사위의 개수.java
@@ -1,6 +1,10 @@
 class Solution {
     public int solution(int[] box, int n) {
-        int answer = (box[0]/n) * (box[1]/n) * (box[2]/n);
-        return answer;
+
+        int availableX = (box[0] - box[0] % n) / n;
+        int availableY = (box[1] - box[1] % n) / n;
+        int availableZ = (box[2] - box[2] % n) / n;
+
+        return availableX * availableY * availableZ;
     }
 }
