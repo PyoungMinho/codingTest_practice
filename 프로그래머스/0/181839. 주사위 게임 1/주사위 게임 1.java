1b950416e9682b407874bd7990cbea6f11ed00e5
Pyoung MinHo <149047097+PyoungMinho@users.noreply.github.com>
2024-08-17 15:00:15 +0900

    [level 0] Title: 주사위 게임 1, Time: 0.05 ms, Memory: 77 MB -BaekjoonHub

--- 프로그래머스/0/181839. 주사위 게임 1/주사위 게임 1.java
+++ 프로그래머스/0/181839. 주사위 게임 1/주사위 게임 1.java
@@ -1,13 +1,5 @@
 class Solution {
     public int solution(int a, int b) {
-        int answer = 0;
-        if(a%2==1 && b%2==1){
-            answer = a * a + b * b;
-        }
-        else if (a % 2 == 1 || b % 2 == 1){
-            answer = 2 * (a + b);
-        }
-        else answer = Math.abs(a-b);
-        return answer;
+        return (a%2==1&&b%2==1?a*a+b*b:(a%2==1||b%2==1?2*(a+b):(a>b?a-b:b-a)));
     }
 }
