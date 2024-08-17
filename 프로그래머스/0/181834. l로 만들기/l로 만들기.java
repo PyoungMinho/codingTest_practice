ae271958754db742dc48b9ea06db740558d00338
Pyoung MinHo <149047097+PyoungMinho@users.noreply.github.com>
2024-08-17 14:59:32 +0900

    [level 0] Title: l로 만들기, Time: 19.53 ms, Memory: 85.3 MB -BaekjoonHub

--- 프로그래머스/0/181834. l로 만들기/l로 만들기.java
+++ 프로그래머스/0/181834. l로 만들기/l로 만들기.java
@@ -1,15 +1,5 @@
 class Solution {
     public String solution(String myString) {
-        StringBuilder answer = new StringBuilder();
-        
-        for(int i = 0; i < myString.length(); i++){
-            char currentChar = myString.charAt(i);
-            if(currentChar < 'l'){
-                answer.append('l');
-            } else {
-                answer.append(currentChar);
-            }
-        }
-        return answer.toString();
+        return myString.replaceAll("[^l-z]", "l");
     }
 }
