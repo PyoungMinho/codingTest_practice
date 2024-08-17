b79a628930ecdbfb640efcf8967cbd877cc87476
Pyoung MinHo <149047097+PyoungMinho@users.noreply.github.com>
2024-08-17 15:00:41 +0900

    [level 0] Title: 가위 바위 보, Time: 0.06 ms, Memory: 77.9 MB -BaekjoonHub

--- 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.java
+++ 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.java
@@ -1,16 +1,22 @@
 class Solution {
     public String solution(String rsp) {
-        String answer = "";
-        String[] rspStr = rsp.split("");
-        
-        for(String s : rspStr){
-            
-            if(s.equals("2")) answer += "0";            
-            else if(s.equals("0")) answer+="5";
-            else answer +="2";
-            
+        char[] cRsp = rsp.toCharArray();
+        StringBuilder answer = new StringBuilder();
+
+        for(int i=0; i < cRsp.length; i++) {
+            switch(cRsp[i]) {
+                case '0' :
+                    answer.append("5");
+                    break;
+                case '2' :
+                    answer.append("0");
+                    break;
+                case '5' :
+                    answer.append("2");
+                    break;
+            }
         }
-        
-        return answer;
+        return answer.toString();
+
     }
 }
