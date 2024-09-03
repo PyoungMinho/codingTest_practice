0a9db9c1ecb1843640749115a8afa342b7618589
Pyoung MinHo <149047097+PyoungMinho@users.noreply.github.com>
2024-09-03 10:48:16 +0900

    [level 0] Title: 7의 개수, Time: 4.50 ms, Memory: 75.5 MB -BaekjoonHub

--- 프로그래머스/0/120912. 7의 개수/7의 개수.java
+++ 프로그래머스/0/120912. 7의 개수/7의 개수.java
@@ -1,20 +1,15 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
 class Solution {
     public int solution(int[] array) {
-        int answer = 0;
-        StringBuilder sb = new StringBuilder();
-        
-        for(int i : array){
-             String str = String.valueOf(i);
-             sb.append(str);
-        }
-        
-        String result = sb.toString();
-        for (int j = 0; j < result.length(); j++) {
-            if (result.charAt(j) == '7') {
-                answer++;
-            }
-        }
-       
-        return answer;
+        return (int) Arrays.stream(
+                        Arrays.stream(array)
+                                .mapToObj(String::valueOf)
+                                .collect(Collectors.joining())
+                                .split("")
+                )
+                .filter(s -> s.equals("7"))
+                .count();
     }
 }
