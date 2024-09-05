8e98bed3c21fd6f624c97d371e38bd02e8241020
Pyoung MinHo <149047097+PyoungMinho@users.noreply.github.com>
2024-09-05 11:21:40 +0900

    [level 0] Title: 겹치는 선분의 길이, Time: 0.04 ms, Memory: 75.4 MB -BaekjoonHub

--- 프로그래머스/0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.java
+++ 프로그래머스/0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.java
@@ -1,15 +1,15 @@
-import java.util.*;
 class Solution {
     public int solution(int[][] lines) {
-        Map<Integer, Integer> map = new HashMap<>();
-        for (int[] line : lines) {
-            int from = Math.min(line[0], line[1]);
-            int to = Math.max(line[0], line[1]);
-            for (int i = from; i < to; i++) {
-                map.merge(i, 1, Integer::sum);
-            }
+        int answer = 0;
+        for (int i = -100; i < 100; i++) {
+            int line = 0;
+            if (lines[0][0]<=i && lines[0][1]>i) line++;
+            if (lines[1][0]<=i && lines[1][1]>i) line++;
+            if (lines[2][0]<=i && lines[2][1]>i) line++;
+
+            if(line > 1) answer++;
         }
 
-        return (int) map.values().stream().filter(i -> i > 1).count();
+        return answer;
     }
 }
