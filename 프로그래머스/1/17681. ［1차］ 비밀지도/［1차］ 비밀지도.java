6ff31d51b46a373a1985d30a43630885e8a620b1
Pyoung MinHo <149047097+PyoungMinho@users.noreply.github.com>
2024-09-09 16:23:30 +0900

    [level 1] Title: [1차] 비밀지도, Time: 2.38 ms, Memory: 89.6 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -1,51 +1,16 @@
-import java.util.*;
-
 class Solution {
-    public String[] solution(int n, int[] arr1, int[] arr2) {
-        String[][] mergedArray = mergeArrays(n, arr1, arr2);
-        String[] result = new String[n];
-        
-        for (int i = 0; i < n; i++) {
-            StringBuilder sb = new StringBuilder();
-            for (int j = 0; j < n; j++) {
-                if (mergedArray[i][j].equals("#")) {
-                    sb.append("#");
-                } else {
-                    sb.append(" ");
-                }
-            }
-            result[i] = sb.toString();
-        }
-        
-        return result;
-    }
-    
-    public String[][] mergeArrays(int n, int[] arr1, int[] arr2) {
-        String[][] mergedArray = new String[n][n];
-        
-        for(int i = 0; i < n; i++) {
-            String binary1 = toBinaryString(arr1[i], n);
-            String binary2 = toBinaryString(arr2[i], n);
-            for(int j = 0; j < n; j++) {
-                if(binary1.charAt(j) == '1' || binary2.charAt(j) == '1') {
-                    mergedArray[i][j] = "#";
-                } else {
-                    mergedArray[i][j] = " ";
-                }
-            }
-        }
-        
-        return mergedArray;
-    }
-    
-    public String toBinaryString(int num, int length) {
-        String binary = Integer.toBinaryString(num);
-        StringBuilder sb = new StringBuilder();
-        
-        for(int i = binary.length(); i < length; i++) {
-            sb.append("0");
-        }
-        sb.append(binary);
-        return sb.toString();
-    }
+  public String[] solution(int n, int[] arr1, int[] arr2) {
+      String[] answer = new String[n];
+      String temp;
+
+      for(int i = 0 ; i < n ; i++){
+          temp = String.format("%16s", Integer.toBinaryString(arr1[i] | arr2[i]));
+          temp = temp.substring(temp.length() - n);
+          temp = temp.replaceAll("1", "#");
+          temp = temp.replaceAll("0", " ");
+          answer[i] = temp;
+      }
+
+      return answer;
+  }
 }
