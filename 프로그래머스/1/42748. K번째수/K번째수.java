2becfb57fb1bd95b88255a56cf0e6c718cc6a7a3
Pyoung MinHo <149047097+PyoungMinho@users.noreply.github.com>
2024-09-09 16:22:37 +0900

    [level 1] Title: K번째수, Time: 0.93 ms, Memory: 73.3 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -1,19 +1,13 @@
-import java.util.*;
-
+import java.util.Arrays;
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
-        int[] answer = new int[commands.length]; 
-            
-        for (int i = 0; i < commands.length; i++) {
-            int start = commands[i][0]; 
-            int end = commands[i][1]; 
-            int k = commands[i][2]; 
+        int[] answer = new int[commands.length];
 
-            int[] answerArr = Arrays.copyOfRange(array, start - 1, end);
-            Arrays.sort(answerArr); 
-            answer[i] = answerArr[k - 1]; 
+        for(int i=0; i<commands.length; i++){
+            int[] temp = Arrays.copyOfRange(array, commands[i][0]-1, commands[i][1]);
+            Arrays.sort(temp);
+            answer[i] = temp[commands[i][2]-1];
         }
-
 
         return answer;
     }
