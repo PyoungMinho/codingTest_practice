2afce7d292e3b3ce17676a0e8d723f47c90a4ace
Pyoung MinHo <149047097+PyoungMinho@users.noreply.github.com>
2024-10-22 12:15:20 +0900

    [level 2] Title: 올바른 괄호, Time: 7.16 ms, Memory: 53.1 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,21 +1,14 @@
-import java.util.*;
-    
 class Solution {
     boolean solution(String s) {
-      if(s.charAt(0) == ')') return false; // 무조건 false 인 경우
-
-    Stack<Character> stack = new Stack<>();
-
-    try{
-      for (char c : s.toCharArray()) {
-        if(c == '(') stack.push(c);
-        else if(c == ')') stack.pop(); // size 0에서 pop하면 exeception터짐
-      }
-    }catch (Exception e){
-      return false;
-    }
-
-
-    return stack.size() == 0 ? true : false;
+        int cnt = 0;
+        
+        for(int i=0;i<s.length();i++){
+            
+            if(s.charAt(i)=='('){ cnt++; } 
+            else { cnt--; }
+            
+            if(cnt<0)  return false;
+        }
+        return cnt>0?false:true;
     }
 }
