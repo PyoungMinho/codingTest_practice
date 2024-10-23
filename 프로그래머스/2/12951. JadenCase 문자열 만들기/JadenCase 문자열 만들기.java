d526e14ad4705443130021f156e731336b4d0b6f
Pyoung MinHo <149047097+PyoungMinho@users.noreply.github.com>
2024-10-23 11:17:01 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 44.50 ms, Memory: 84.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
@@ -0,0 +1,19 @@
+class Solution {
+public String solution(String s) {
+   String answer = "";
+   String str = " " + s;
+
+
+    for (int i = 1; i < str.length(); i++){
+        
+        if (str.charAt(i - 1) == ' '){
+          answer += Character.toUpperCase(str.charAt(i)); 
+        } else {
+          answer += Character.toLowerCase(str.charAt(i));
+        }
+        
+    }
+
+    return answer;
+  }
+}
