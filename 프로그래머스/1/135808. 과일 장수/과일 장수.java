7b6046e183bc7aef646b77c8ff37c37a4f6f1f0b
Pyoung MinHo <149047097+PyoungMinho@users.noreply.github.com>
2025-01-09 14:52:45 +0900

    [level 1] Title: 과일 장수, Time: 61.87 ms, Memory: 140 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/과일 장수.java
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.java
@@ -2,18 +2,18 @@ import java.util.*;
 
 class Solution {
     public int solution(int k, int m, int[] score) {
-        int answer = 0;
-        int boxCnt = (int)score.length / m;
-        int totalCnt = boxCnt*m;
+        int answer = 0; // 최대 이익을 저장할 변수 초기화
         
-        int idx = score.length - totalCnt;
+        // 사과 점수를 오름차순으로 정렬 (가장 낮은 점수부터 높은 점수 순으로 정렬)
+        Arrays.sort(score);
 
-        Arrays.sort(score);        
-        while(idx<totalCnt){
-            answer += score[idx] * m;
-            idx +=m;
+        // 사과 배열을 뒤에서부터 m개씩 그룹으로 나누어 반복
+        // (가장 높은 점수부터 m개씩 상자를 구성하고 반복)
+        for (int i = score.length; i >= m; i -= m) { 
+            // 현재 상자의 최저 점수(score[i - m]) * 상자의 크기(m)를 더해 이익 계산
+            answer += score[i - m] * m;
         }
 
-        return answer;
+        return answer; // 계산된 최대 이익 반환
     }
 }
