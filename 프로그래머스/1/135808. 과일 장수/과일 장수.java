6b08e7bc715862495224352b1fa02e0958dd27b3
Pyoung MinHo <149047097+PyoungMinho@users.noreply.github.com>
2025-01-09 14:41:37 +0900

    [level 1] Title: 과일 장수, Time: 86.83 ms, Memory: 141 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/과일 장수.java
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.java
@@ -3,11 +3,15 @@ import java.util.*;
 class Solution {
     public int solution(int k, int m, int[] score) {
         int answer = 0;
+        int boxCnt = (int)score.length / m;
+        int totalCnt = boxCnt*m;
+        
+        int idx = score.length - totalCnt;
 
-        Arrays.sort(score);
-
-        for(int i = score.length; i >= m; i -= m){
-            answer += score[i - m] * m;
+        Arrays.sort(score);        
+        while(idx<totalCnt){
+            answer += score[idx] * m;
+            idx +=m;
         }
 
         return answer;
