0afd7f9e1ff14c881c10d7b1f240cff7765d0a5b
Pyoung MinHo <149047097+PyoungMinho@users.noreply.github.com>
2025-01-23 11:26:52 +0900

    [level 1] Title: 소수 찾기, Time: 17.04 ms, Memory: 81.9 MB -BaekjoonHub

--- 프로그래머스/1/12921. 소수 찾기/소수 찾기.java
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.java
@@ -1,21 +1,27 @@
-class Solution {
-  public int solution(int n) {
-      int answer = 0;
-            
-      for(int i = 2; i <= n; i++){
-          boolean chk = true;
-          for(int j = 2; j*j <= i; j++){ 
-              if(i % j == 0){
-                  chk = false;
-                  break;
-              }
-          }
-          if(chk){
-              answer++;
-          }
-      }
-      
-     return answer;           
-      
-  }
+public class Solution { 
+    public static int solution(int n) { // 메서드 선언
+        if (n < 2) return 0; // n이 2보다 작으면 소수가 없음
+
+        boolean[] isPrime = new boolean[n + 1]; // 소수 여부를 저장하는 배열
+        for (int i = 2; i <= n; i++) {
+            isPrime[i] = true; // 처음에는 모든 숫자를 소수로 가정
+        }
+
+        for (int i = 2; i * i <= n; i++) { // i의 배수를 지움
+            if (isPrime[i]) { // i가 소수인 경우
+                for (int j = i * i; j <= n; j += i) { // i의 배수를 모두 false로 설정
+                    isPrime[j] = false;
+                }
+            }
+        }
+
+        int answer = 0; // 소수의 개수 카운트
+        for (int i = 2; i <= n; i++) {
+            if (isPrime[i]) {
+                answer++;
+            }
+        }
+
+        return answer;
+    }
 }
